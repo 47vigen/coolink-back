2f07926f99153e8d43aecbf15668849c9815069d
Vigen <vigenpooya@gmail.com>
2021-09-16 13:47:35 +0430

    add type arg

--- src/graphql/upload/schema.js
+++ src/graphql/upload/schema.js
@@ -4,6 +4,6 @@ export const schema = gql`
   scalar Upload
 
   type Mutation {
-    uploadImage(pk: String!, image: Upload!): String!
+    uploadImage(type: UploadType!, pk: String!, image: Upload!): String!
   }
 `
