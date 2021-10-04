182edf93bd6a85d90b410c201f83ff6fc596b0ca
Vigen <vigenpooya@gmail.com>
2021-10-04 13:37:17 +0330

    add email resolvers

--- src/graphql/auth/resolvers.js
+++ src/graphql/auth/resolvers.js
@@ -1,5 +1,6 @@
-import { sign, signRefresh } from '../../services/jwt'
+import { sign, signRefresh, signEmail, verifyEmail } from '../../services/jwt'
 import { notFound, throwError } from '../../services/response'
+import { sendMail } from '../../services/email'
 import User from '../user/model'
 
 const login = (_, { userInput: { email, password } }, ctx) =>
@@ -20,8 +21,34 @@ const login = (_, { userInput: { email, password } }, ctx) =>
     )
     .catch(throwError())
 
+const confirmEmail = (_, { token }, ctx) =>
+  verifyEmail(token)
+    .then(({ id }) => User.findById(id))
+    .then(notFound())
+    .then((user) => (user ? Object.assign(user, { role: 'USER_CONFIRMED' }).save() : null))
+    .then((user) =>
+      signRefresh(user.id)
+        .then((tokenRefresh) => {
+          ctx.reply.setCookie('refresh', tokenRefresh, {
+            httpOnly: true,
+            secure: true
+          })
+          return true
+        })
+        .then(() => sign(user.id).then((token) => ({ token, user: user.view(true) })))
+    )
+    .catch(throwError())
+
+const sendConfirmEmail = (_, object, { auth }) =>
+  signEmail(auth?.user?.id)
+    .then((token) => sendMail(auth?.user?.email, 'ایمیل خود را تایید کنید', `https://coolink.ir/confirm/${token}`))
+    .then(() => true)
+    .catch(() => throwError())
+
 export const resolvers = {
   Mutation: {
-    login
+    login,
+    confirmEmail,
+    sendConfirmEmail
   }
 }
