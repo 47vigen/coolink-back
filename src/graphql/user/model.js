5ee4fb09894d281ae09d811b4e9395a52d157a53
Vigen <vigenpooya@gmail.com>
2021-10-04 13:38:05 +0330

    add user confirmed role

--- src/graphql/user/model.js
+++ src/graphql/user/model.js
@@ -4,7 +4,7 @@ import mongoose, { Schema } from 'mongoose'
 import mongooseKeywords from 'mongoose-keywords'
 import { env } from '../../config'
 
-const roles = ['USER', 'ADMIN']
+const roles = ['USER', 'USER_CONFIRMED', 'ADMIN']
 
 const userSchema = new Schema(
   {
