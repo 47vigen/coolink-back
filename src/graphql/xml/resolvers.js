b4807a73fa609789bac1e2595792a1f26b57c941
Vigen <vigenpooya@gmail.com>
2021-12-01 22:48:07 +0330

    complete show xml for feeds

--- src/graphql/xml/resolvers.js
+++ src/graphql/xml/resolvers.js
@@ -1,16 +1,54 @@
 import { Page } from '../page'
 import { Post } from '../blog/post'
+import { Section } from '../section'
+import { Feed } from '../instagram/feed'
+import { isArray } from '../../utils/is'
 import { notFound, throwError } from '../../services/response'
 
 const show = (_, args, ctx) =>
-  Page.find()
-    .sort({ createdAt: -1 })
-    .then(async (pages) => [...pages, ...(await Post.find().sort({ createdAt: -1 }))])
+  Page.find(null, null, { sort: { updatedAt: -1 } })
+    .then((pages) =>
+      Promise.all(
+        pages.map((page) =>
+          Section.findOne({ page: page.id, type: 'feeds' })
+            .then((section) => {
+              if (!!section) {
+                return Feed.find({ pagePk: page.pk }, null, { sort: { createdAt: -1 }, limit: 12 })
+                  .then((feeds) =>
+                    feeds?.map((feed) => ({ loc: `https://coolink.ir/${page.slug}/feeds/${feed.pk}`, priority: 0.5, updatedAt: feed.updatedAt }))
+                  )
+                  .then((feeds) =>
+                    feeds?.length
+                      ? [page, ...feeds, { loc: `https://coolink.ir/${page.slug}/feeds`, priority: 0.5, updatedAt: feeds[0].updatedAt }]
+                      : [page]
+                  )
+                  .catch(throwError(null, page))
+              } else return page
+            })
+            .catch(throwError(null, page))
+        )
+      ).catch(
+        throwError(
+          null,
+          pages.map((page) => [page])
+        )
+      )
+    )
+    .then((pages) => {
+      const items = []
+      pages.map((page) => (isArray(page) ? items.push(...page) : items.push(page)))
+      return items
+    })
+    .then((pages) =>
+      Post.find(null, null, { sort: { updatedAt: -1 } })
+        .then((posts) => [...pages, ...posts])
+        .catch(() => pages)
+    )
     .then(notFound('xml data not found'))
     .then((dynamics) =>
       dynamics?.map((dynamic) => ({
-        loc: dynamic.body ? `https://coolink.ir/blog/${dynamic.slug}` : `https://coolink.ir/${dynamic.slug}`,
-        priority: dynamic.body ? 0.7 : 1,
+        loc: dynamic.loc || (dynamic.body ? `https://coolink.ir/blog/${dynamic.slug}` : `https://coolink.ir/${dynamic.slug}`),
+        priority: dynamic.priority || (dynamic.body ? 0.7 : 1),
         lastmod: new Date(dynamic.updatedAt).toISOString(),
         changefreq: 'daily'
       }))
